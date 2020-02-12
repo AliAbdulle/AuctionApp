f78ef32ccb5a85df45879be4f13579b471b7cf61
AliAbdulle <alidhobey@gmail.com>
2020-02-11 20:58:02 -0600

    create controller auction

--- AuctionApp/AuctionApp/Models/Auction.cs
+++ AuctionApp/AuctionApp/Models/Auction.cs
@@ -7,5 +7,22 @@ namespace AuctionApp.Models
 {
     public class Auction
     {
+        public long Id { get; set; }
+        
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndtTime { get; set; }
+
+
+        public Decimal StartPrice { get; set; }
+
+        public Decimal? CurrentPrice { get; set; }
+
+
     }
 }
