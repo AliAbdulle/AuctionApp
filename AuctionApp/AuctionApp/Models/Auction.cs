5ee44c14fb7d370c23767a43e17191ef43c965cd
AliAbdulle <alidhobey@gmail.com>
2020-02-13 21:21:10 -0600

    changed required field

--- AuctionApp/AuctionApp/Models/Auction.cs
+++ AuctionApp/AuctionApp/Models/Auction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,19 +9,20 @@ namespace AuctionApp.Models
     public class Auction
     {
         public long Id { get; set; }
-
+        [Required ]
         public string Category { get; set; }
-        
+        [Required]
         public string Title { get; set; }
 
         public string Description { get; set; }
 
         public string ImageUrl { get; set; }
-
+        [Required]
         public DateTime StartTime { get; set; }
+        [Required]
         public DateTime EndTime { get; set; }
 
-
+        [Required]
         public Decimal StartPrice { get; set; }
 
         public Decimal? CurrentPrice { get; set; }
